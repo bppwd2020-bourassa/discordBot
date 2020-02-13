37d3be5a71174ac6af3411e7432910d1e8f5a463
bppwd2020-bourassa <j.bourassa@baypath.net>
2020-02-12 19:33:30 -0500

    update

--- commands/info/ip.js
+++ commands/info/ip.js
@@ -5,8 +5,7 @@ const { RichEmbed } = require("discord.js"); module.exports = {
     usage: "<output>",
     run: (client, message, args) => {
         message.delete();
-	message.reply('The ip to the vanilla server is **3.81.133.186**')
-	message.reply('The ip to the modded server is **3.90.233.242**')
-	message.reply('The link to the dropbox is https://www.dropbox.com/sh/6t6u5q2gf16d8qe/AACQKmY0DdqLaQYLsEDdq6qfa?dl=0')
+	message.reply('The ip to Jareds server is **52.1.71.35**')
+	message.reply('The ip to Teagans server is **54.210.134.82**')
     }
 }
